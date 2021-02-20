4c9a382d17ee334cbcb4f7afdddfd7e24c6da914
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2021-02-19 18:27:24 -0600

    Initial commit

---
+++ protein_synthesis.py
@@ -0,0 +1,25 @@
+#Author: Benjamin Hawkins
+#Date: 19 February 2021
+
+#Objects: Ribosome, Amino Acid, mRNA, Template DNA strand, RNA Polymerase
+
+#Program Structure
+#1. Accept user input for DNA strand (starting with ATG?)
+#2. Accept user input for template DNA strand
+#3. RNAPolymerase object converts the DNA strand into the cooresponding mRNA
+
+
+class AminoAcid:
+    #def __init__(self)
+    #Attributes
+    #Name, abbreviation, polarity, codons for call
+    pass
+
+class Ribosome:
+    pass
+
+class RNAPolymerase:
+    pass
+
+class TemplateDNA:
+    pass
