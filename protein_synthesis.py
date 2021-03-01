dc4d879d5acd7ca1d6a951704bd469ce3d44a3c0
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2021-03-01 15:31:38 -0600

    definition of AminoAcid dataclass

--- protein_synthesis.py
+++ protein_synthesis.py
@@ -1,6 +1,9 @@
 #Author: Benjamin Hawkins
 #Date: 1 March 2021
 
+#Importation of dependencies
+from dataclasses import dataclass
+
 #----------Definition of classes----------
 class NucleicAcid:
     NUCLEIC_ACID_TYPES  = ("Template DNA", "Coding DNA", "mRNA")
@@ -11,6 +14,9 @@ class NucleicAcid:
         else:
             self.__type = type
         self.__sequence = sequence
+        self.__numOfBasePairs = len(sequence)
+        self.__numOfCodons = int(self.__numOfBasePairs / 3)
+        self.toMRNA()
     
     def toMRNA(self):
         if self.__type == "Template DNA":
@@ -20,12 +26,27 @@ class NucleicAcid:
         if self.__type == "mRNA":
             pass
 
+@dataclass
+class AminoAcid:
+    __name : str
+    __abbreviation : str
+    __class : str
+    __polarity : str
+    __charge : str
+    __molecular_mass : float
+    __codons : []
+
+
+
+    
+
 #----------Implementation of Program----------
 
-#Re
+#Request user input for the type of nucleic acid and the base pair sequence to be operated on
 nucleicAcidType = input("Is the entry a selection of [Template DNA], [Coding DNA], or [mRNA]? ")
 print(nucleicAcidType)
-entrySequence = input(f"Please enter the sequence of {nucleicAcidType}: ")
-print(entrySequence)
+basePairSequence = input(f"Please enter the sequence of {nucleicAcidType}: ")
+print(basePairSequence)
 
-nucleicAcidInput = NucleicAcid(nucleicAcidType, entrySequence)
+#Instantiate 
+nucleicAcidInput = NucleicAcid(nucleicAcidType, basePairSequence)
